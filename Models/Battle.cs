600c6ccb75a03c1235ef2a17e0f4f240a20e1dc9
blaze5445 <blaze5445@tutamail.com>
2024-08-01 23:59:57 +0300

    Add files via upload

---
+++ Models/Battle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ChampionshipOfBox.Models
+{
+    [Table("Battle")]
+    public class Battle
+    {
+        public int Id { get; set; }
+        public DateTime BattleDateTime { get; set; }
+        public string Winner { get; set; }
+        public string Loser { get; set; }
+        public int RefereePoints { get; set; }
+    }
+}
---
+++ Models/Chempionsip.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ChampionshipOfBox.Models
+{
+    [Table("Chempionsip")]
+    public class Chempionsip
+    {
+        public int Ranking { get; set; }
+        [Key]
+        public string Name { get; set; }
+        public int NumberOfBattle { get; set; }
+    }
+}

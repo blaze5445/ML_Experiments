7db07421d48c649ccbff8cfb1179bcfefa28f64e
blaze5445 <blaze5445@tutamail.com>
2024-08-02 00:00:28 +0300

    Add files via upload

---
+++ DataModel/BattleContext.cs
@@ -0,0 +1,21 @@
+﻿using ChampionshipOfBox.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ChampionshipOfBox.DataModel
+{
+    public class BattleContext : DbContext
+    {
+        public BattleContext() :
+            base("BattleDB")
+        {
+            Database.SetInitializer<BattleContext>(null);
+        }
+
+        public DbSet<Battle> Battle { get; set; }
+        public DbSet<Chempionsip> Chempionsip { get; set; }
+    }
+}
